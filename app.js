11f70bdf2d6a0afb52e496edcbe8e5ab27aff881
Jeanet Lin <linett@gmail.com>
2022-01-25 18:18:05 +0100

    iteratio 3 step 1 & 2 done - css incomplete

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 
+const bodyParser = require("body-parser");
 const express = require("express");
 const hbs = require("hbs");
 
@@ -39,7 +40,7 @@ app.get("/", (req, res) => {
 
 //query to artist search
 app.get("/artist-search", (req, res) => {
-  const artistName = req.query.artists; //--> get the artist from the spotifyAPI
+  const artistName = req.query.searchInput; //--> get the artist from the spotifyAPI
 
   spotifyApi
     .searchArtists(artistName) //--> query the artist
@@ -47,6 +48,7 @@ app.get("/artist-search", (req, res) => {
       let artistList = artistName.body.artists.items; //get the data from the API on artist and what info they have
       //console.log('The received data from the API: ', data.body);
       // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+      console.log(artistList)
       res.render("artist-search-results", { artistList }); //artistList is in the artist-search-results.hbs file, where we display the results
     })
     .catch((err) =>
